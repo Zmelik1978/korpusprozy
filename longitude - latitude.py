a9d5e8a298320f26f32516ee462f2159f72097b5
Richard <80571221+Zmelik1978@users.noreply.github.com>
2024-06-28 08:59:12 +0200

    doplněna možnost zobrazit lokaci na mapě - důležité pro kontrolu správnosti souřadnic

--- longitude - latitude.py
+++ longitude - latitude.py
@@ -1,5 +1,10 @@
 from geopy.geocoders import Nominatim
 from termcolor import colored
+import plotly.express as px
+import pandas as pd
+
+# Jako město zaddej název města
+# Jako místo zadej např. ulici
 
 input_city = input(colored("Zadej město: ", "blue"))
 input_location = input(colored("Zadej místo: ", "blue"))
@@ -9,3 +14,11 @@ location = geolocator.geocode(f"{input_city}, {input_location}")
 
 print(colored("Latitude: ", "green"), location.latitude)
 print(colored("Longitude: ", "green"), location.longitude)
+
+df = pd.DataFrame({"lat": location.latitude, "lon": location.longitude}, index=[0])
+
+# mapa je centralizována na Prahu
+fig = px.scatter_mapbox(df, lat='lat', lon='lon', center={'lat':50.073658, 'lon':14.418540}, color_discrete_sequence=["fuchsia"], zoom=10)
+fig.update_layout(mapbox_style='open-street-map')
+fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+fig.show()
