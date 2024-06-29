1cb5b0ab7527cc01c6217606ee5a253f41af4428
Richard Změlík <80571221+Zmelik1978@users.noreply.github.com>
2024-06-29 17:42:31 +0200

    doplněn script

--- longitude - latitude.py
+++ longitude - latitude.py
@@ -12,13 +12,18 @@ input_location = input(colored("Zadej místo: ", "blue"))
 geolocator = Nominatim(user_agent="user_agent")
 location = geolocator.geocode(f"{input_city}, {input_location}")
 
-print(colored("Latitude: ", "green"), location.latitude)
-print(colored("Longitude: ", "green"), location.longitude)
+if location:
+    
+    print(colored("Latitude: ", "green"), location.latitude)
+    print(colored("Longitude: ", "green"), location.longitude)
 
-df = pd.DataFrame({"lat": location.latitude, "lon": location.longitude}, index=[0])
+    df = pd.DataFrame({"lat": location.latitude, "lon": location.longitude}, index=[0])
 
-# mapa je centralizována na Prahu
-fig = px.scatter_mapbox(df, lat='lat', lon='lon', center={'lat':50.073658, 'lon':14.418540}, color_discrete_sequence=["fuchsia"], zoom=10)
-fig.update_layout(mapbox_style='open-street-map')
-fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-fig.show()
+    # mapa je centralizována na Prahu
+    fig = px.scatter_mapbox(df, lat='lat', lon='lon', center={'lat':50.073658, 'lon':14.418540}, color_discrete_sequence=["fuchsia"], zoom=10)
+    fig.update_layout(mapbox_style='open-street-map')
+    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    fig.show()
+
+else:
+    print(colored("Nejsou k dispozici souřadnice.", "red"))
