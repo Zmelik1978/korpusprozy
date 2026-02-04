ce089f13f1fe070faf21c1fc711dfc1b86c3acaa
Storzer <storzer.martin@seznam.cz>
2026-02-04 13:40:07 +0100

    doplněna Moderní Magdaléna doplněny texty bez toponym

--- arbes_gis.js
+++ arbes_gis.js
@@ -923,7 +923,10 @@ function showGraph(){
                 ]},
                 ]});
                 break;
-        case "První noc u mrtvoly":
+        //case "Sladký hřích" - nenalezena žádná toponyma
+        //case "Lampičky" - nenalezena žádná toponyma
+        //case "Elegie o černých očích" - nenalezena žádná toponyma
+        case "Sběhlé švícko":
             Highcharts.mapChart('gis', {chart: {margin: 0}, title: {text: ''}, navigation: {buttonOptions: {align: 'left', theme: {stroke: '#e6e6e6'}}},mapNavigation: {enabled: true, buttonOptions: {alignTo: 'spacingBox'}}, mapView: {center: [14.418540, 50.0874654], zoom: 10}, tooltip: {useHTML: true, headerFormat: "{point.point.name}", pointFormat: '<br/><img src="{point.image}"/>'}, legend: {enabled: true, title: {text: 'Zázračná madona'}, align: 'right', symbolWidth: 20, symbolHeight: 20, itemStyle: {textOutline: '1 1 1px rgba(255,255,255)'}, backgroundColor: 'rgba(255,255,255,0.8)', float: true, borderColor: '#e6e6e6', borderWidth: 1, borderRadius: 2, itemMarginBottom: 5}, plotOptions: {mappoint: {dataLabels: {enabled: false}}}, series: [{type: 'tiledwebmap', name: '', provider: {type: 'OpenStreetMap'}, showInLegend: false}, 
                 // existující místa
                 {type: 'mappoint', name: 'existing place', marker: {symbol: 'url(../img/placeholder.png)', width: 24, height: 24,}, data: [
@@ -937,6 +940,17 @@ function showGraph(){
                 {name: 'Hostinec U Eggenbergu (v budově bývalého letohrádku Eggenberg)', lon: 14.406091, lat: 50.078346, image: "../foto_praha/storzer_eggenberg.JPG"}]},
                 {name: 'Baráky', lon: 0, lat: 0, image: "../foto_praha/DOPLNIT_OBRAZEK.jpg"}
                 ]});
+                break;
+        case "Moderní Magdaléna":
+            Highcharts.mapChart('gis', {chart: {margin: 0}, title: {text: ''}, navigation: {buttonOptions: {align: 'left', theme: {stroke: '#e6e6e6'}}},mapNavigation: {enabled: true, buttonOptions: {alignTo: 'spacingBox'}}, mapView: {center: [14.418540, 50.0874654], zoom: 10}, tooltip: {useHTML: true, headerFormat: "{point.point.name}", pointFormat: '<br/><img src="{point.image}"/>'}, legend: {enabled: true, title: {text: 'Zázračná madona'}, align: 'right', symbolWidth: 20, symbolHeight: 20, itemStyle: {textOutline: '1 1 1px rgba(255,255,255)'}, backgroundColor: 'rgba(255,255,255,0.8)', float: true, borderColor: '#e6e6e6', borderWidth: 1, borderRadius: 2, itemMarginBottom: 5}, plotOptions: {mappoint: {dataLabels: {enabled: false}}}, series: [{type: 'tiledwebmap', name: '', provider: {type: 'OpenStreetMap'}, showInLegend: false}, 
+                // existující místa
+                {type: 'mappoint', name: 'existing place', marker: {symbol: 'url(../img/placeholder.png)', width: 24, height: 24,}, data: [
+                    {name: 'Lysá na Šumavě © Jan Otčenášek', lon: 14.0353 , lat: 48.8486, image: "../foto_praha/storzer_lysa.jpg"}
+                // neexistující místa
+                ]},
+                {type: 'mappoint',name: 'vanished place', marker: {symbol: 'url(../img/maps-and-flags.png)', width: 24, height: 24}, data: [
+                ]},
+                ]});    
                 break;
         
         default:
